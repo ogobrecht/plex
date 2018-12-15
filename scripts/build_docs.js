ff632f32b028e3571c9c29abfba0e5ffa608db91
Ottmar Gobrecht <ottmar.gobrecht@gmail.com>
2018-12-15 12:05:49 +0100

    update ploc version

--- scripts/build_docs.js
+++
@@ -1,6 +0,0 @@
-var ploc = require('ploc');
-var inFilePattern  = process.argv[2]; // first command line parameter
-var outFilePattern = process.argv[3]; // second one
-var minItemsForToc = process.argv[4]; // third one
-
-ploc.files2docs(inFilePattern, outFilePattern, minItemsForToc)
